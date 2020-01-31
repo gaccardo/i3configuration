d56d45bfbdb1caf6a7af94bc74b3cb516bea6af6
Guido Accardo <gaccardo@gmail.com>
2020-01-31 14:11:49 -0300

    awx current context

---
+++ snow/pystatus/awx.py
@@ -0,0 +1,23 @@
+import os
+import configparser
+
+
+class Py3status:
+
+    def __get_current_profile(self):
+        home = os.environ.get("HOME")
+        config = configparser.ConfigParser()
+        config.read('{}/.aws/credentials'.format(home))
+
+        return str(config.get("default", "name"))
+
+    def awx(self):
+        return {
+            'full_text': self.__get_current_profile(),
+            'cached_until': self.py3.time_in(5)
+        }
+
+
+if __name__ == "__main__":
+    p3 = Py3status()
+    print p3.awx()
