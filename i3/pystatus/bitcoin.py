0ba2f770121075505760fbddd960b88a28c3c8b4
Guido Accardo <gaccardo@gmail.com>
2020-03-18 11:35:37 -0300

    more

---
+++ i3/pystatus/bitcoin.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class Py3status:
+
+    def bitcoin(self):
+        rr = requests.get("https://blockchain.info/ticker")
+        data = rr.json()
+        return {
+            'full_text': "${}, ${:.2f}, ${:.2f}".format(
+                data['USD']['last'],
+                data['USD']['last'] * 0.27485289,
+                data['USD']['last'] * 0.27485289 * 75
+            ),
+            'cached_until': self.py3.time_in(300)
+        }
+
