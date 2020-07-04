94abd9e8eb6ff93dab8a6eafed083c9caceca227
Guido Accardo <gaccardo@gmail.com>
2020-07-04 11:31:55 -0300

    updated fish config

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -4,32 +4,49 @@
 alias aws="$HOME/.pyenv/versions/awscli/bin/aws"
 
 # Kubernetes
+alias awx="awx | fzf --ansi | xargs -I {} awx {}"
 alias kx="kubectx"
 alias kn="kubens"
 alias kk="kubectl"
 alias ls="exa"
 alias ek="eksctl"
 alias mkk="microk8s.kubectl"
+alias s="ssh"
+
+function k8s
+  set k8s_status "off"
+  if test $argv = "on"
+    set k8s_status "yes"
+  else
+    set k8s_status "no"
+  end
+
+  set -g theme_display_k8s_namespace $k8s_status
+  set -g theme_display_k8s_context $k8s_status
+end
+
+function ddt
+  if test $theme_display_date = no
+      set new_status yes
+  else
+      set new_status no
+  end
+  set -g theme_display_date $new_status
+end
 
 # Terminal
-alias icat="kitty +kitten icat "
-alias kdiff="kitty +kitten diff "
 alias i3conf="vi ~/.config/i3/config"
 alias pdf="/usr/bin/xdg-open"
-alias py3="python3"
-#alias s="sshx"
-alias m="mosh"
-#alias ssh="s"
 alias rr="ranger"
 alias mc="mc -S nicedark"
-alias zs="vi ~/.zshrc"
 alias ls="exa --header"
 alias ll="exa --long --header --git"
 alias tree="exa --tree"
 alias vi="vim"
+alias bt="bashtop"
 
 function vf
-  du -a . | awk '{print $2}' | fzf | xargs -r $EDITOR
+  du -a . | awk '{print $2}' | fzf --ansi | xargs -r $EDITOR
 end
 
 # EXA
