56b31bddcfacfabe87a222a57c1061d295ea289a
Guido Accardo <gaccardo@gmail.com>
2020-07-04 11:48:47 -0300

    updated qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,1 +1,35 @@
-c.fonts.monospace = 'Envy Code R'
+import glob
+import os.path
+
+c.aliases = {
+        'q': 'quit',
+        'w': 'session-save',
+        'wq': 'quit --save',
+        'x': 'quit --save',
+        'h': 'help'
+        }
+
+c.confirm_quit = ['downloads']
+c.new_instance_open_target = 'tab-silent'
+c.new_instance_open_target_window = 'last-focused'
+c.auto_save.session = True
+
+c.url.searchengines = {
+        'ap': 'https://archlinux.org/packages/?q={}',
+        'aur': 'https://aur.archlinux.org/packages.php?K={}',
+        'ar': 'https://wiki.archlinux.org/?search={}',
+        'DEFAULT': 'https://duckduckgo.com/?q={}',
+        'ec': 'https://www.ecosia.org/search?q={}',
+        'gi': 'https://github.com/search?q={}',
+        'ji': 'http://jisho.org/search/{}',
+        'ra': 'https://rateyourmusic.com/search?searchtype=a&searchterm={}',
+        'wikt': 'https://en.wiktionary.org/wiki/Special:Search?search={}',
+        'wi': 'https://en.wikipedia.org/wiki/Special:Search?search={}',
+        'yo': 'https://youtube.com/results?search_query={}'
+        }
+
+c.fonts.default_family = "Envy Code R"
+
+config.bind("xb", "config-cycle statusbar.hide")
+config.bind("xt", "config-cycle tabs.show always switching")
+config.bind("xx", "config-cycle statusbar.hide ;; config-cycle tabs.show always switching")
