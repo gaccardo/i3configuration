430ac11eba1455ba52ea578066277a626de00fcc
Guido Accardo <gaccardo@gmail.com>
2020-08-04 14:16:57 -0300

    minor changes

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -68,3 +68,4 @@ function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
   ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
 end
 
+alias keepassxc="firejail keepassxc"
--- i3/pystatus/bitcoin.py
+++ i3/pystatus/bitcoin.py
@@ -7,11 +7,14 @@ class Py3status:
         rr = requests.get("https://blockchain.info/ticker")
         data = rr.json()
         return {
-            'full_text': "${}, ${:.2f}, ${:.2f}".format(
-                data['USD']['last'],
-                data['USD']['last'] * 0.27485289,
-                data['USD']['last'] * 0.27485289 * 125
+            'full_text': "${}".format(
+                data['USD']['last']
             ),
+            #'full_text': "${}, ${:.2f}, ${:.2f}".format(
+            #    data['USD']['last'],
+            #    data['USD']['last'] * 0.27485289,
+            #    data['USD']['last'] * 0.27485289 * 140
+            #),
             'cached_until': self.py3.time_in(300)
         }
 
