527d6def3c2c485faba39880b88d7370c5ba7ad1
Guido Accardo <gaccardo@gmail.com>
2020-08-22 19:44:00 -0300

    qtile machine for vogel

---
+++ qtile/machines/vogel/config.py
@@ -0,0 +1,220 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import os
+import subprocess
+from libqtile.config import Key, Screen, Group, Drag, Click
+from libqtile.lazy import lazy
+from libqtile import layout, bar, widget, hook
+
+from typing import List  # noqa: F401
+
+mod = "mod4"
+
+keys = [
+    # Switch between windows in current stack pane
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "k", lazy.layout.down()),
+    Key([mod], "j", lazy.layout.up()),
+    Key([mod], "l", lazy.layout.right()),
+
+    # max window
+    Key([mod], "f", lazy.window.toggle_fullscreen()),
+
+    # Move windows up or down in current stack
+    Key([mod, "control"], "h", lazy.layout.shuffle_left()),
+    Key([mod, "control"], "k", lazy.layout.shuffle_down()),
+    Key([mod, "control"], "j", lazy.layout.shuffle_up()),
+    Key([mod, "control"], "l", lazy.layout.shuffle_right()),
+
+    # Switch window focus to other pane(s) of stack
+    #Key([mod], "space", lazy.layout.next()),
+    Key([mod], "space", lazy.spawn("rofi -show run -config ~/.config/i3/rofi/rofi.conf -font 'Envy Code R 12'")),
+
+    # Swap panes of split stack
+    Key([mod, "shift"], "space", lazy.layout.rotate()),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
+    Key([mod], "Return", lazy.spawn("alacritty --config-file /home/guido/.alacritty/alacritty.yml")),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout()),
+    Key([mod], "w", lazy.window.kill()),
+
+    Key([mod], "r", lazy.restart()),
+    Key([mod, "control"], "q", lazy.shutdown()),
+    #Key([mod], "r", lazy.spawncmd()),
+
+    Key([mod, "shift"], "x", lazy.spawn("i3lock -i /home/guido/Pictures/Wallpapers/lock.png -t -f")),
+    # mediakeys
+    Key([mod], "F11", lazy.spawn("pamixer --sink 1 -i 5")),
+    Key([mod], "F10", lazy.spawn("pamixer --sink 1 -d 5")),
+    Key([mod], "F9", lazy.spawn("pamixer -t")),
+    Key([mod], "d", lazy.spawn("nautilus")),
+#    Key([], "Super_L", lazy.spawn("i3lock")),
+
+]
+
+groups = [Group(i) for i in "1234567890"]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen()),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+    ])
+
+# layout common settings
+common_settings = {
+  "border": 0,
+  "border_normal": "#262626",
+  "border_focus": "#262626",
+  "border_normal": "#262626",
+  "border_normal": "#262626",
+}
+
+layouts = [
+    layout.Max(),
+    layout.Stack(num_stacks=4),
+    # Try more layouts by unleashing below layouts.
+    #layout.Bsp(),
+    layout.Columns(**common_settings),
+    layout.Matrix(**common_settings),
+    layout.MonadTall(**common_settings),
+    layout.MonadWide(**common_settings),
+    #layout.RatioTile(),
+    #layout.Tile(),
+    layout.TreeTab(
+        **common_settings,
+        font="Envy Code R",
+        font_size="12",
+        bg_color="#323232",
+        active_bg="#606060",
+        inactive_bg="#404040",
+        margin_left=0,
+        padding_left=0,
+        panel_width=100,
+        sections=["tabs"]
+    ),
+    #layout.VerticalTile(),
+    #layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='Envy Code R',
+    fontsize=12,
+    padding=1,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(),
+                widget.CurrentLayout(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.CPU(),
+                widget.Net(),
+                #widget.TextBox("default config", name="default"),
+                widget.BitcoinTicker(),
+                widget.Battery(),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                #widget.QuickExit(),
+            ],
+            18,
+            background="#262626"
+        ),
+    ),
+    Screen(
+        top=bar.Bar([], 18)
+    ),
+    Screen(
+        top=bar.Bar([], 18)
+    ),
+]
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
+
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~/.bin/nm-applet-reset.sh')
+    subprocess.call([home])
+    sound = os.path.expanduser('~/.bin/pasystray-reset.sh')
+    subprocess.call([sound])
