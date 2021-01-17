9aaf8bfc09d3b277061b7b3e15ce73078cbbf161
Guido Accardo <gaccardo@gmail.com>
2021-01-17 19:56:37 -0300

    added an amount calculator to right_bottom bar

---
+++ i3/pystatus/amount.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import pickle
+
+
+class Py3status:
+
+    def bitcoin(self):
+        rr = requests.get("https://blockchain.info/ticker")
+        data = rr.json()
+        full_text = "${:.2f}".format(data['USD']['last']*0.33545028)
+        return {
+            'full_text': full_text,
+            'cached_until': self.py3.time_in(300),
+        }
+
+
+if __name__ == "__main__":
+    config = {
+        'always_show': True,
+    }
+    from py3status.module_test import module_test
+    module_test(Py3status, config=config)
