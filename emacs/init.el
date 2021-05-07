0de24179bee340bbdc911b50efd039f2ec9a29d4
Guido Accardo <gaccardo@gmail.com>
2021-05-07 10:35:39 -0300

    emacs ignore C-j

--- emacs/init.el
+++ emacs/init.el
@@ -246,3 +246,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(add-hook 'org-mode-hook (lambda () (electric-indent-local-mode -1)))
