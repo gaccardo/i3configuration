a13c9c48e1c909dbf3cf00b1030cd841e947d2f3
Guido Accardo <gaccardo@gmail.com>
2022-07-17 13:12:13 -0300

    improved how value is retrieved

--- i3/scripts/machines/snow/vol_down.sh
+++ i3/scripts/machines/snow/vol_down.sh
@@ -4,7 +4,7 @@ sinks=$(pactl list | grep "Sink #" | awk '{print $2}' | sed 's/#//')
 for sink in $sinks;
 do
     pactl set-sink-volume $sink -5%
+    VOLUME=`pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $sink + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'`
 done
 
-VOLUME=`pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'`
 notify-send -t 350 "Volume control" "Volume: $VOLUME %"
