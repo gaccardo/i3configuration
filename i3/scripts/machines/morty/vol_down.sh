7a2fcc731510add50dae198bffc8d24343497820
Guido Accardo <gaccardo@gmail.com>
2023-04-14 15:42:58 -0300

    motry

---
+++ i3/scripts/machines/morty/vol_down.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pactl set-sink-volume 0 -5%
+pactl set-sink-volume 1 -5%
+pactl set-sink-volume 3 -5%
+
+VOLUME=`pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'`
+
+notify-send -t 350 "Volume control" "Volume: $VOLUME %"
